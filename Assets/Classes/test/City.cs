52ee89eabea4e5d51e5e183748cc6b370e051f62
Jonathan Longman <jonathan.longman@live.com.au>
2019-03-09 16:38:19 +0000

    Classes into Assets

---
+++ Assets/Classes/test/City.cs
@@ -0,0 +1,8 @@
+namespace healthHack
+{
+    public class City
+    {
+        private int id;
+        private SIRModel model;
+    }
+}
---
+++ Assets/Classes/test/ModelClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+namespace healthHack 
+{
+    public interface IModelInterface
+    {
+        void Update();
+        bool AddVacine();
+        bool AddAntiVaxxer();
+        bool DrugPopulation();
+        double GetCosts();
+        double GetInfected();
+        double GetSubseptible();
+        double GetDead();
+        double GetRecovered();
+    }
+}
+
