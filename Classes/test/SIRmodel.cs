527c226424572ee23fd7cbfcaf9b417c3d61c83f
Jonathan Longman <jonathan.longman@live.com.au>
2019-03-09 16:23:53 +0000

    SIR Model

--- Classes/test/SIRmodel.cs
+++ Classes/test/SIRmodel.cs
@@ -11,14 +11,29 @@ namespace healthHack
         private float recovered;
         private float vaccinated;
 
+
+
+
+        private int time;
+        private float proportion_treated;
         private float treatment_reduction;
         private float background_transmission_rate;
-        private int recovery_untreated;
-        private int recovery_treated;
+        private float recovery_untreated;
+        private float recovery_treated;
 
-        public SIRModel()
+        public SIRModel(float susceptible, float infected_U, float treatment_reduction, float background_transmission_rate, int recovery_untreated, int recovery_treated)
         {
+            this.susceptible = susceptible;
+            this.infected_U = infected_U;
+            this.infected_TR = 0;
+            this.recovered = 0;
+            this.vaccinated = 0;
 
+            this.proportion_treated = 0;
+            this.treatment_reduction = treatment_reduction;
+            this.background_transmission_rate = background_transmission_rate;
+            this.recovery_treated = recovery_treated;
+            this.recovery_untreated = recovery_untreated;
         }
 
         public float change_in_susceptible()
@@ -28,7 +43,12 @@ namespace healthHack
 
         public float getProportionTreated()
         {
-            return 0;
+            return this.proportion_treated;
+        }
+
+        public void setProportionTreated(float proportionTreated)
+        {
+            this.proportion_treated = proportionTreated;
         }
 
         public float change_in_infected_U()
@@ -53,10 +73,40 @@ namespace healthHack
             float i_tr = this.change_in_infected_TR();
             float r = this.change_in_recovered();
 
+            this.time += 1;
             this.susceptible += s;
             this.infected_U += i_u;
             this.infected_TR += i_tr;
             this.recovered += r;
+        }
+
+        public void display()
+        {
+            Console.WriteLine("\nDay: " + this.time);
+            Console.WriteLine("Sus: " + this.susceptible);
+            Console.WriteLine("Inf_U: " + this.infected_U);
+            Console.WriteLine("Inf_TR: " + this.infected_TR);
+            Console.WriteLine("Recov: " + this.recovered);
+        }
+
+
+        static int Main(string[] args)
+        {
+            SIRModel model = new SIRModel(500000, 1, (float)0.5, (float)0.000001, 14, 7);
+            model.display();
+            for (int i = 0; i < 50; i++)
+            {
+                if (i == 20)
+                {
+                    model.setProportionTreated((float)0.3);
+                }
+                model.update();
+                model.display();
+            }
+
+            Console.WriteLine("Enter to exit...");
+            Console.ReadLine();
+            return 0;
         }
     }
 }
